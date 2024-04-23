4d28f4c2d50790f79245113056fd05f6b04be4f3
nishika678 <147605762+nishika678@users.noreply.github.com>
2024-04-24 00:32:55 +0530

    Update ShopCategory.jsx

--- src/Pages/ShopCategory.jsx
+++ src/Pages/ShopCategory.jsx
@@ -7,7 +7,7 @@ const ShopCategory=(props)=> {
   const {all_product}=useContext(ShopContext);
   return (
     <div className='shop-category'>
-<img src={props.banner} alt="" />
+<img className='shopcategory-banner'src={props.banner} alt="" />
 <div className="shopcategory-indexSort">
   <p>
     <span>Showing 1-12</span> out of 36 products
@@ -23,7 +23,11 @@ return <Item key={i} id={item.id} name={item.name} image={item.image} new_price=
 else{
   return null;
 }
-})}</div>
+})}
+</div>
+<div className="shopcategory-loadmore">
+  Explore More
+</div>
     </div>
   )
 }
